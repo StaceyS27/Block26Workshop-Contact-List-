bd624d1584d1225d00daa85ae5a914aba8423c52
Stacey Sanchez <staceysanchez@macbook-air.mynetworksettings.com>
2023-09-14 12:31:51 -0400

    selectedcontactID state create

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import './App.css'
 import ContactList from './components/ContactList'
 
 function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
   
 
   return (
     <>
+    {selectedContactId ? (
+      <div>Selected Contact View</div>
+    ) : (
       <ContactList />
+    )}
+
     </>
   )
 }

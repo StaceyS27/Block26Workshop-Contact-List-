72bb0af5c9350f14103fbd9f68145d579c7cd207
Stacey Sanchez <staceysanchez@macbook-air.mynetworksettings.com>
2023-09-13 23:44:39 -0400

    unused imports removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ContactList from './components/ContactList'
 import dummyContacts from './components/ContactList'

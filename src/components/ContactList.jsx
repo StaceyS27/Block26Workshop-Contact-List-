76fb82ab1f5d63e330dd2522a2e45eb43e333e0b
Stacey Sanchez <staceysanchez@macbook-air.mynetworksettings.com>
2023-09-14 11:35:40 -0400

    fetching contacts from api

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactRow from './contactRow'
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -12,7 +13,25 @@ const dummyContacts = [
 
 export default function ContactList() {
 const [contacts, setContacts] = useState(dummyContacts);
-  console.log("Contacts: ", contacts)
+  console.log("Contacts: ", contacts);
+
+  useEffect(()=> {
+    async function fetchContacts() {
+        try {
+            const response = await fetch(
+                "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+            );
+            const result = await response.json();
+            setContacts(result);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    fetchContacts();
+  }, []);
+
+  console.log(contacts)
+
 
 
     return (

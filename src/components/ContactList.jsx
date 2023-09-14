f5c926b493abfb030c7c52429c2ea9f720ecb835
Stacey Sanchez <staceysanchez@macbook-air.mynetworksettings.com>
2023-09-13 23:58:23 -0400

    added tr tag to head

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ContactRow from './contactRow' 
+import ContactRow from './contactRow'
 
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
     { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
     { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-  ];
+];
 
 
 
@@ -14,7 +14,9 @@ export default function ContactList() {
     return (
         <table>
             <thead>
-                <th colspan="3"> Contact List </th>
+                <tr>
+                    <th colspan="3"> Contact List </th>
+                </tr>
             </thead>
             <tbody>
                 <tr>
